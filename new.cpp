64df5cf88002f8ac68c68d3655248c425836ef41
Erfan Nosrati <erfannosrati20@gmail,com>
2021-03-01 12:15:30 +0330

    Initial commit

---
+++ new.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+void swap(int *&pa)
+{
+    int a = 5;
+    pa = &a;    
+    return ;
+}
+
+int main()
+{
+    int *pa = NULL;
+    swap(pa);
+    cout << pa;
+}
